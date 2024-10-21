12d63e45ebfccf7696d21f3a042633216f5b8da9
Shubham <shubhuu1804@gmail.com>
2024-10-21 16:49:05 +0530

    Initialized the repository and Setup Logging and Custom Exception Handling

---
+++ lib/libraries.py
@@ -0,0 +1,3 @@
+import sys
+from src.BlogBuddyAI import logger
+from src.BlogBuddyAI.exception import CustomException
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "BlogBuddyAI"
+AUTHOR_USER_NAME = "mShubham18"
+SRC_REPO = "BlogBuddyAI"
+AUTHOR_EMAIL = "shubhuu1804@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="BlogBuddyAI: Your Smart Companion for Effortless Blogging",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
