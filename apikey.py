adcee57fb9b510ed01b9d4c2b312fd05f926b389
Shubham <shubhuu1804@gmail.com>
2024-10-22 16:11:49 +0530

    Working on API KEY Secrecy

--- apikey.py
+++ apikey.py
@@ -2,6 +2,6 @@
 #limewire_api_key="lmwr_sk_ubVsrmnSsK_bdyvN9XG9v1btyykYQrnfOAjDkWKN2PrcKQ3k"
 #google_gemini_api_key="AIzaSyAViZZVNOTDl_MsLDPDbEZS6R1Ppv47e8I"
 #UNSPLASH_ACCESS_KEY="qHqLXNdO_MUi16tNwNYwpbGcDFAK4u4nP687Ewps62Y"
-
+import os
 google_gemini_api_key = os.getenv('GOOGLE_GEMINI_API_KEY')
 UNSPLASH_ACCESS_KEY = os.getenv('UNSPLASH_ACCESS_KEY')
